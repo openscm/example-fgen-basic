87cea35affb00582f9465accd29b644b74a07ca8
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2025-08-25 16:48:30 +0200

    Add Fortran back-end

---
+++ src/example_fgen_basic/error_v/creation.f90
@@ -0,0 +1,44 @@
+!> Error creation
+!>
+!> A very basic demo to get the idea.
+!
+! TODO: discuss - we should probably have some convention for module names.
+! The hard part is avoiding them becoming too long...
+module m_error_v_creation
+
+    use m_error_v, only: ErrorV, NO_ERROR_CODE
+
+    implicit none
+    private
+
+    public :: create_error
+
+contains
+
+    function create_error(inv) result(err)
+        !! Create an error
+        !!
+        !! If an odd number is supplied, the error code is no error (TODO: cross-ref).
+        !! If an even number is supplied, the error code is 1.
+        !! If a negative number is supplied, the error code is 2.
+
+        integer, intent(in) :: inv
+        !! Value to use to create the error
+
+        type(ErrorV) :: err
+        !! Created error
+
+        if (inv < 0) then
+            err = ErrorV(code=2, message="Negative number supplied")
+            return
+        end if
+
+        if (mod(inv, 2) .eq. 0) then
+            err = ErrorV(code=1, message="Even number supplied")
+        else
+            err = ErrorV(code=0)
+        end if
+
+    end function create_error
+
+end module m_error_v_creation
