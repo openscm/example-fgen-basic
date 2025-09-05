5722fdb2190c4cc219008371f2dd19405bf5c741
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2025-09-05 21:38:32 +0200

    Add notes and allow env var configuration

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,16 @@ See https://docs.pytest.org/en/7.1.x/reference/fixtures.html#conftest-py-sharing
 """
 
 import os
-import sys
 
-if os.environ.get("CI", "false") == "true" and sys.platform == "win32":
-    os.add_dll_directory("C:\\mingw64\\bin")
+if dll_directory_to_add := os.environ.get("PYTHON_ADD_DLL_DIRECTORY", None):
+    # Add the directory which has the libgfortran.dll file.
+    #
+    # A super deep dive into this is here:
+    # https://stackoverflow.com/a/78276248
+    # The tl;dr is - mingw64's linker can be tricked by windows craziness
+    # into linking a dynamic library even when we wanted only static links,
+    # so if you want to avoid this, link with something else (e.g. the MS linker).
+    # (From what I know, this isn't an issue for the built wheels
+    # thanks to the cleverness of delvewheel)
+    os.add_dll_directory(dll_directory_to_add)
+    # os.add_dll_directory("C:\\mingw64\\bin")
