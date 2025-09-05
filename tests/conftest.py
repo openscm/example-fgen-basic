24f5ad49215b46b1e9d808739fe8d3c64c9c29a0
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2025-09-05 19:42:54 +0200

    Try workaround for windows

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,3 +3,9 @@ Re-useable fixtures etc. for tests
 
 See https://docs.pytest.org/en/7.1.x/reference/fixtures.html#conftest-py-sharing-fixtures-across-multiple-files
 """
+
+import os
+import sys
+
+if os.environ["CI"] == "true" and sys.platform() == "win32":
+    os.add_dll_directory("C:\\ProgramData\\mingw64\\mingw64\\bin")
